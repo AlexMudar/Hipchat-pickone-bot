ae125c74f4b2c85b9c32520637a97667bcd8107e
AMudar <AlexMudar@QuickenLoans.com>
2016-02-01 17:46:04 -0500

    my first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pick One
+
+A very simple hipchat bot that picks a word at random out of a list.
---
+++ index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+	response.render('pages/index');
+});
+
+ app.post('/test',function(request,response){
+	var messageText;
+	var messageTextArray = [];
+	var json;
+	var randomWord;
+	var randomNumber;
+	
+	messageText = request.body["item"]["message"]["message"].toString();
+	messageTextArray = messageText.split(" ");
+	randomNumber = Math.floor((Math.random() * messageTextArray.length) + 1);
+	randomWord = messageTextArray[randomNumber];
+	
+	json = JSON.stringify({"color":"gray","message":randomWord,"notify":false,"message_format":"text"});
+	
+	response.writeHead(200, {"Content-Type": "application/json"});
+	response.end(json);
+});
+
+/*			
+	var bodyJson = JSON.parse(body);
+	var athleteID = bodyJson[a]["athlete"]["id"].toString();
+	athleteRanking.push( {id: athleteID, distanceInMiles: distanceInMiles, name: name});
+	stravaResponse = stravaResponse + (athleteRanking[c].name).toString() + ": " + ((athleteRanking[c].distanceInMiles).toFixed(1)).toString() + "mi \r"; 
+	response.writeHead(200);
+	response.end();
+*/
+	
+app.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
+});
+
+
